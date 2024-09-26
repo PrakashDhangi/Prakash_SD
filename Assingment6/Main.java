cf7726f1c29cad09314f0e418544fe1b59779963
Prakash <prakashdhangi6@gmail.com>
2024-09-26 20:29:09 +0530

    6th assingment

---
+++ Assingment6/Main.java
@@ -0,0 +1,26 @@
+package Assingment6;
+
+public class Main {
+    public static void main(String[] args) {
+       //approach-1
+        Animal a=new Animal();
+        Cat c=new Cat();
+        Dog d=new Dog();
+        Lion l=new Lion();
+
+        c.sound();
+        d.sound();
+        l.sound();
+
+        //approach-2
+      Animal[] a1=new Animal[3];
+
+        a1[0]=new Cat();
+        a1[1]=new Dog();
+        a1[2]=new Lion();
+
+
+
+
+    }
+}

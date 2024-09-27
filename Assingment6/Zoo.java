4114644271045371d0c3d0f3c57fc241018d9936
Prakash <prakashdhangi6@gmail.com>
2024-09-27 08:29:52 +0530

    6th assingment

--- Assingment6/Zoo.java
+++ Assingment6/Zoo.java
@@ -2,24 +2,34 @@ package Assingment6;
 
 public class Zoo {
     public static void main(String[] args) {
-       //approach-1
-        Animal a=new Animal();
-        Cat c=new Cat();
-        Dog d=new Dog();
-        Lion l=new Lion();
+        //approach-1
+        Animal a = new Animal();
+        Cat c = new Cat();
+        Dog d = new Dog();
+        Lion l = new Lion();
 
         c.sound();
         d.sound();
         l.sound();
 
         //approach-2
-      Animal[] a1=new Animal[3];
-        a1[0]=new Cat();
-        a1[1]=new Dog();
-        a1[2]=new Lion();
+        Animal[] a1 = new Animal[3];
+        a1[0] = new Cat();
+        a1[1] = new Dog();
+        a1[2] = new Lion();
+
+        for (int i = 0; i <= a1.length - 1; i++) {
+            a1[i].sound();
 
 
+            //Approach3
+            Animal[] animals = {new Cat(), new Dog(), new Lion()};
+            for (Animal animal : animals) {
+                animal.sound();
 
+            }
 
+        }
     }
 }
+
